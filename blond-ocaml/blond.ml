77bbeaf5c300f6a04620a3294c360d4da171eee3
Kalarus <notcad@keemail.me>
2025-05-01 21:31:06 -0400

    finished core, starting init env values

--- blond-ocaml/blond.ml
+++ blond-ocaml/blond.ml
@@ -1,41 +1,88 @@
+(******************************************************************************)
+(***************************** Types ******************************************)
+(******************************************************************************)
+exception Error of string * string * value (* location, message, cause *)
+type const =
+  | NumConst of int
+  | StringConst of string
+  | BoolConst of bool
+
 type exp =
-  | Const of const
-  | Var of var
-  | App of applicable * exp list
+  | ConstExp of const
+  | VarExp of var
+  | ListExp of exp list
+
 type value =
-  | NumVal of int
-  | StringVal of string
+  | ConstVal of const
   | VarVal of var
-  | ListVal of value list
   | FunVal of applicable
+  | ListVal of value list
+  
 type answer =
   | ValAns of value
   | PairAns (* {_|_} *)
 type env = (var list * value list) list
-type cont = value -> meta-cont -> answer
+type cont = value -> meta_cont -> answer
 type meta_cont = (env * cont) list
+(*
 type re_env =
   | UnitEnv of unit -> value
   | VarEnv of var -> value
   | VarValEnv of var -> value -> value
-type lambdaAbs = (value list) -> cont -> meta_cont -> answer
+ *)
+type lambdaAbsBody = (value list) -> cont -> meta_cont -> answer
+  type fsubrBody = (exp list) -> env -> cont -> meta_cont -> value
+type lambdaAbs = int * lambdaAbsBody
 type subr =
   | ThunkSubr of unit -> value
   | UnarySubr of value -> value
   | BinarySubr of value -> value -> value
   | TernarySubr of value -> value -> value -> value
-type fsubr = (exp list) -> env -> cont -> meta-cont -> value
+type fsubr = int * fsubrBody
+
 type delta_reifier = value -> value -> value -> env -> cont -> meta_cont -> answer
 type gamma_reifier = value -> value -> value -> cont -> meta_cont -> answer
 type applicable =
   | Abs of lambdaAbs
   | Subr of subr
   | FSubr of fsubr
-  | ReifiedEnv of re_env
+  | ReifiedEnv of env
   | ReifiedCont of cont
   | Delta of delta_reifier
   | Gamma of gamma_reifier
 
+let rec _exp_up_star (elst : exp list) : value list =
+  List.map _exp_up elst
+and _exp_up (e : exp) : value =
+  match e with
+  | ConstExp c -> ConstVal c
+  | VarExp var -> VarVal var
+  | ListExp elst -> ListVal (_exp_up_star elst)
+let _env_up (env : env) : value = FunVal (ReifiedEnv env) 
+let _cont_up (cont : cont) : value = FunVal (ReifiedCont cont)
+
+let _exp_down_star (vlst : value list) : exp list =
+  List.map _exp_down vlst
+and _exp_down v : exp =
+  match v with
+  | ConstVal c -> Const c
+  | VarVar var -> Var var
+  | ListVal vlst -> ListExp elst
+  | FunVal f -> Const 0 (* undefined *)
+let _env_down v : env =
+  match v with
+  | FunVal (ReifiedEnv env) -> env
+  | _ -> []
+let _cont_down v : cont =
+  match v with
+  | FunVal (ReifiedCont cont) -> cont
+  | _ -> (fun x _ -> x)
+
+(******************************************************************************)
+(*************************** Application Dispatches ***************************)
+(******************************************************************************)
+
+(******************* Primitive Functions & Special Forms **********************)
 (* reducing primitive functions *)
 let _apply_subr (fo : subr) args env cont tau : value =
   match fo, args with
@@ -57,14 +104,15 @@ let _apply_subr (fo : subr) args env cont tau : value =
      let cont''' = (fun a1 tau ->
          _eval arg2 env (cont'' a1) tau) in
      _eval arg1 env cont''' tau
-  | _,_ -> _wrong '_apply_subr "arity mismatch" args
+  | _,_ -> Error ("_apply_subr", "arity mismatch", ListVal args)
 
 let _apply_fsubr (fv : fsubr) args env cont tau : value =
-  let arity = (_fetch-arity fv) in
+  let (arity,fv) = fv in
   if arity = (List.length args) || arity = 0
   then fv args env cont tau
-  else _wrong '_apply_fsubr "arity mismatch" args
+  else Error ("_apply_fsubr", "arity mismatch", ListVal args)
 
+(******************* Procedures/Lambda Abstractions **********************)
 let cons_list_val v vlst =
   match vlst with
   | ListVal lst -> ListVal (v :: vlst)
@@ -77,30 +125,153 @@ let _evlis args env cont tau : value =
      let cont'' = (fun v tau ->
          (_evlis args_rst env (cont' v) tau)) in
      _eval arg1 env cont'' tau
-let _apply_procedure (p : fsubr) args env cont tau : value =
-  if (_fetch-arity p) = (List.length args)
+let _apply_procedure (p : lambdaAbs) args env cont tau : value =
+  if (_fetch_arity p) = (List.length args)
   then
     let cont' = (fun lv tau -> p lv k tau) in
     _evlis args env cont' tau
-  else _wrong '_apply_procedure "arity mismatch" args
-(*
-in scheme implementation, reified env somehow is env,
-the function turning reified env into env is just the identity function
- *)
-let _apply_environment (f : re_env) args env cont tau : value =
-  match f, args with
-  | UnitEnv func, [] ->
-     cont (func ()) tau
-  | VarEnv func, arg1 :: [] ->
+  else Error ("_apply_procedure", "arity mismatch", ListVal args)
+
+(******************* Applying Environment **********************)
+
+(* unfinished *)
+let _L_lookup_common var env : value list =
+  match env with
+  | [] -> [] (* undefined in scheme implementation *)
+  | (vars,vals) :: env_rst ->
+     let pos = _index var table_common_identifiers in
+     if pos >= 0
+     then ((set-car! (vars,vals) (var :: vars)); (* append var to vars*)
+           (set-cdr! (vars,vals) (* append _access (_nth pos table_common_values) to vals*)
+                  (, vals));
+           vals)
+     else []
+let _L_lookup var env : value list =
+  match env with
+  | [] -> [] (* undefined in scheme implementation *)
+  | (vars,vals) :: env_rst ->
+     let position = _index var vars in
+     if position >= 0 then _nth_lst position vals
+     else
+       if env_rst = [] then _L_lookup_common var env
+       else _L_lookup var env_rst
+let _apply_environment_set i v env_f cont tau : value =
+  match i with
+  | VarVal var ->
+     let location = _L_lookup var (_env_down env_f) in
+     if location = []
+     then Error ("_apply_environment", "undefined variable", i)
+     else (_update location v;
+           cont (List.fst location) tau)                     
+  | _ -> Error ("_apply_environment", "not an identifier", i)
+       
+let _R_lookup_common var : value =
+  match _index var table_common_identifiers with
+  | Some pos ->
+     _nth pos table_common_values
+  | None -> ConstVal 0 (* undefined behaviour in scheme implementation *)
+let _R_lookup var env =
+  match env with
+  | [] -> _R_lookup_common var
+  | (vars,vals) :: env_rst ->
+     let position = _index var vars in
+     if position >= 0 then _nth position vals
+     else _R_lookup var env_rst
+let env_to_value env : value =
+  List.map (fun (vars,vals) -> (List.map VarVal vars), vals) env
+let _apply_environment (f : env) args env cont tau : value =
+  match args with
+  | [] ->
+     cont (env_to_value f) tau
+  | arg1 :: [] ->
      let cont' =
        (fun i tau ->
          match i with
-         | VarVal var -> cont (_R_lookup var (_env-down f)) tau
-         | _ -> _wrong '_apply_environment "not an identifier" i) in
+         | VarVal var -> cont (_R_lookup var f) tau
+         | _ -> Error ("_apply_environment", "not an identifier", i)) in
      _eval arg1 env cont' tau
-  | VarValEnv func, arg1 :: arg2 :: [] ->
-  | _,_ -> _wrong '_apply_environment "arity mismatch" args
-let _apply (fo : applicable) args env cont tau =
+  | arg1 :: arg2 :: [] ->
+     let cont' = (fun var v tau ->
+         _apply_environment_set var v f cont tau) in
+     let cont'' = (fun var tau ->
+         _eval arg2 env (cont' var) tau) in
+     _eval arg1 env cont'' tau
+  | _ -> Error ("_apply_environment", "arity mismatch", ListVal args)
+(******************* Applying Continuations **********************)
+let _apply_continuation_jumpy cont_r args env cont tau : value =
+  match args with
+  | arg1 :: [] ->
+     _eval arg1 env (_cont_down cont_r) tau
+  | _ -> Error ("_apply_continuation_jumpy", "arity mismatch" args)
+let _apply_continuation_pushy cont_r args env cont tau : value =
+  match args with
+  | arg1 :: [] ->
+     _eval arg1 env (_cont_down cont_r) (_meta_push env cont tau)
+  | _ -> Error ("_apply_continuation_pushy", "arity mismatch" args)
+val _apply_continuation = ref _apply_continuation_jumpy
+(******************* Applying Reifiers **********************)
+
+let _apply_delta (d : delta_reifier) args env cont tau =
+  d (_exp_up_star args) (_env_up env) (_cont_up cont)
+    (_top_env tau) (_top_cont tau) (_meta_pop tau)
+let _apply_gamma (g : gamma_reifier) args env cont tau =
+  g (_exp_up_star args) (_env_up env) (_cont_up cont)
+    (_top_cont tau) (_meta_pop tau)
+
+(******************************************************************************)
+(********************* Helpers for Spawning New Levels ************************)
+(******************************************************************************)
+
+let _terminate_level (v : value) tau : value =
+  (_top-cont tau) v (_meta-pop tau)
+let _check_cont_spawn (exp_rfl : exp) (env_rfl : env)
+      (cont_rfl : applicable) env cont tau : value =
+  match cont_rfl with
+  | Subr (UnarySubr func) ->
+     let cont' = (fun a tau -> _terminate-level (func a) tau) in
+     _eval exp_rfl env_rfl cont' (_meta-push env cont tau)
+  | FSubr (1,func) ->
+     func [exp_rfl] env_rfl _terminate-level (_meta-push env cont tau)
+  | Abs (1,func) ->
+     let cont' = (fun a tau ->
+         func [a] (_top-cont tau) (_meta-pop tau)) in
+     _eval exp_rfl env_rfl cont' (_meta-push env cont tau)
+  | ReifiedEnv env_re ->
+     let cont' =
+       (fun i tau ->
+         match i with
+         | VarVal var -> _terminate-level (_R_lookup var (_env-down _k)) tau
+         | _ -> Error ("environment", "not an identifier", i)) in
+     _eval exp_rfl env_rfl cont' (_meta-push r k tau)
+  | ReifiedCont cont_re ->
+     _eval exp_rfl env_rfl (_cont-down cont_re) (_meta-push env cont tau)
+  | Delta d ->
+     d (_exp-up exp_rfl) (_env-up env_rfl)
+       (_cont-up _terminate-level) env cont tau
+  | Gamma g ->
+     g (_exp-up exp_rfl) (_env-up env_rfl)
+       (_cont-up _terminate-level) cont tau
+  | Abs _ -> Error ("_meaning", "pitfall lambda abs/proc", cont_rfl) 
+  | Subr _ -> Error ("_meaning", "pitfall subr", cont_rfl) 
+  | FSubr _ -> Error ("_meaning", "pitfall fsubr", cont_rfl) 
+(* a1 has to be expressible to type check*)
+let _check_and_spawn (a1 : value) (a2 : value) (a3 : value) env cont tau : value =
+  match a2, a3 with
+  | FunVal (ReifiedEnv a2_env), FunVal a3_f ->
+     _check_cont_spawn (_exp-down a1) a2_env a3_f env cont tau
+  | _ -> Error ("_meaning", "polluted environment or pitfall due to not fun", ListVal [a2, a3])
+       
+let _meaning args env cont tau : value =
+  match args with
+  | arg1 :: arg2 :: arg3 :: args_rst ->
+     let cont' =  (fun a1 a2 a3 tau ->
+         _check_and_spawn a1 a2 a3 r k tau) in
+     let cont'' = (fun a1 a2 tau -> _eval arg3 env (cont' a1 a2) tau) in
+     let cont''' = (fun a1 tau -> _eval arg2 env (cont'' a1) tau) in
+     _eval arg1 env cont''' tau
+  | _ -> ConstVal (NumConst 0) (* undefined *)
+(******************* The Main Apply Function **********************)
+let _apply_h (fo : applicable) args env cont tau =
   match fo with
   | Abs func -> _apply_procedure func args env cont tau
   | Subr func -> _apply_subr func args env cont tau
@@ -109,12 +280,15 @@ let _apply (fo : applicable) args env cont tau =
   | ReifiedCont func -> _apply_continuation func args env cont tau
   | Delta func -> _apply_delta func args env cont tau
   | Gamma func -> _apply_gamma func args env cont tau
- 
+let _apply (f_val : value) args env cont tau =
+  match f_val with
+  | FunVal fo -> _apply_h fo args env cont tau
+  | _ -> Error ("_apply", "unapplicable form", fo)
 let _lookup_common var cont tau =
-  match _index var table-common-identifiers with
+  match _index var table_common_identifiers with
   | Some pos ->
-     cont (_nth pos table-common-values) tau
-  | None -> _wrong '_lookup_common "unbound identifier" var
+     cont (_nth pos table_common_values) tau
+  | None -> Error ("_lookup_common", "unbound identifier", VarVal var)
 (* 
 _access (_nth position (cdar env))
 to nth num lst
@@ -125,18 +299,21 @@ in scheme implementation, environment has
 at least one argument to start with, but by allowing env to be empty
 we make it more concise
  *)
-let _lookup var env cont tau : value =
+let rec _lookup var env cont tau : value =
   match env with
   | [] -> _lookup_common var cont tau
-  | (vars,exps) :: env_rst ->
+  | (vars,vals) :: env_rst ->
      let position = _index var vars in
-     if position >= 0 then cont (_nth position exps) tau
+     if position >= 0 then cont (_nth position vals) tau
      else _lookup var env_rst cont tau
-let rec _eval (expr : exp) env cont tau = 
+let rec _eval (expr : exp) env cont tau : value= 
   match expr with
-  | Const _ -> cont expr tau
-  | Var var -> _lookup var env cont tau
-  | App fun_exp args ->
-     let cont' = (fun f tau ->
-         (_apply f args env cont tau)) in
-     _eval fun_exp env tau
+  | VarExp var -> _lookup var env cont tau
+  | ConstExp c -> cont (ConstVal c) tau
+  | ListExp [] -> cont (ListVal []) tau
+  | ListExp (fun_exp :: args) ->
+     let cont' = (fun f_val tau ->
+         (_apply f_val args env cont tau)) in
+     _eval fun_exp env cont' tau
+  
+     
---
+++ blond-ocaml/blond_init_env_vals.ml
@@ -0,0 +1,97 @@
+open Blond
+(*
+type applicable =
+  | Abs of lambdaAbs
+  | Subr of subr
+  | FSubr of fsubr
+  | ReifiedEnv of env
+  | ReifiedCont of cont
+  | Delta of delta_reifier
+  | Gamma of gamma_reifier
+ *)
+let _quote : fsubrBody =
+  fun args env cont tau ->
+  cont (List.fst args) tau
+
+let _if : fsubrBody =
+  fun args env cont tau ->
+  match args with
+  | arg :: arg_t :: arg_f :: [] ->
+     let cont' = (fun bval tau ->
+         match bval with
+         | ConstVal (BoolConst b) ->
+            if b then _eval arg_t env cont tau
+            else _eval arg_f env cont tau
+         | ListVal [] -> _eval arg_f env cont tau
+         | _ -> _eval arg_t env cont tau) in
+     _eval arg env cont' tau
+  | _ -> ListVal [] (* undefined case *)
+
+let _lambda : fsubrBody =
+  fun args env cont tau ->
+  match args with
+  | (ListExp paras) :: body :: [] ->
+     let bd = (fun val_lst cont tau ->
+         _eval body (_extend_env paras val_lst env) cont tau) in
+     let lbd = FunVal (Abs (List.length paras,bd)) in
+     cont lbd tau
+  | _ -> ListVal [] (* undefined *)
+
+let fsubr_table_h =
+  [(1, _quote),
+   (3, _if),
+   (2, _lambda)
+  ]
+let subr_table_h =
+  [
+  ]
+let subr_table = List.map Subr subr_table_h
+let fsubr_table = List.map FSubr fsubr_table_h
+(*********)    
+    (_inSubr 1 car) (_inSubr 1 cdr)
+        (_inSubr 1 caar) (_inSubr 1 cadr)
+        (_inSubr 1 cdar) (_inSubr 1 cddr)
+        (_inSubr 1 caddr) (_inSubr 1 cdddr)
+        (_inSubr 1 last-pair)
+        (_inSubr 1 null?) (_inSubr 1 atom?) (_inSubr 1 pair?)
+        (_inSubr 1 number?) (_inSubr 1 string?) (_inSubr 1 symbol?)
+        (_inSubr 1 zero?) (_inSubr 1 add1) (_inSubr 1 sub1)
+        (_inSubr 2 +) (_inSubr 2 -) (_inSubr 2 *)
+        (_inSubr 2 <) (_inSubr 2 <=) (_inSubr 2 >) (_inSubr 2 >=)
+        (_inSubr 2 cons) (_inSubr 2 equal?)
+        (_inSubr 2 =) (_inSubr 1 boolean?)
+        (_inSubr 1 negative?) (_inSubr 1 positive?)
+        (_inSubr 1 _applicable?)
+
+                            
+        (_inFsubr 2 _lambda)
+        (_inFsubr 2 _delta) (_inFsubr 3 _meaning) (_inFsubr 2 _gamma)
+         (_inSubr 3 _ef)
+        (_inFsubr 2 _common-define) (_inFsubr 2 _define)
+        (_inFsubr 2 _set!)
+        (_inFsubr 0 _case)
+        (_inFsubr 0 _and) (_inFsubr 0 _or)
+        (_inFsubr 0 _evlis)
+        (_inSubr 2 set-car!) (_inSubr 2 set-cdr!)
+        (_inFsubr 0 _begin)
+        (_inSubr 1 display) (_inSubr 1 pretty-print)
+        (_inSubr 1 pretty-print) (_inSubr 0 newline)
+        (_inSubr 1 not) (_inSubr 1 length)
+        (_inFsubr 1 _load) (_inFsubr 1 _mute-load) (_inFsubr 0 _read)
+        (_inSubr 1 open-input-file) (_inSubr 1 eof-object?)
+        (_inSubr 1 close-input-port)
+        (_inSubr 0 flush-output-port)
+        (_inFsubr 0 _openloop)
+        (_inFsubr 3 _extend-reified-environment)
+        (_inFsubr 2 _let) (_inFsubr 2 _letrec)
+        (_inFsubr 2 _rec) (_inFsubr 2 _let*)
+        (_inFsubr 0 _cond)
+        (_inFsubr 0 _blond-exit)
+        (_inSubr 0 _reify-new-environment)
+        (_inFsubr 0 _reify-new-continuation)
+        (_inSubr 0 _continuation-mode)
+        (_inSubr 0 _switch-continuation-mode)
+
+(******************************************************************************)
+(***************************** Types ******************************************)
+(******************************************************************************)
